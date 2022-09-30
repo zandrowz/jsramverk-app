e41418e74afb1ae15404ff12d2b33e1076070869
Sandra Karlsson <sakq21@student.bth.se>
2022-09-30 11:06:26 +0100

    kmom03 done

--- src/components/Trix.test.js
+++ src/components/Trix.test.js
@@ -23,61 +23,8 @@ test('renders "select option', () => {
     expect(selectOpt).toBeInTheDocument();
 });
 
-test('renders a specific label text', () => {
+test('renders title input label text', () => {
     render(<Editor />);
     const labelText = screen.getByLabelText("Enter a title:");
     expect(labelText).toBeInTheDocument();
   });
-
-// test('renders a specific label text', () => {
-//     const mockHandleClick = jest.fn();
-//     render(<Editor onClick={mockHandleClick}/>);
-//     const button = screen.getByRole("button", {name: "Create new"});
-//     fireEvent.click(button);
-//     expect(mockHandleClick).toHaveBeenCalledTimes(1);
-//   });
-
-// test('Renders without crashing ', () => {
-//     render(<Editor />);
-//     expect(screen.getByText(/Choose a document/i)).toBeInTheDocument();
-//     expect(screen.getByText(/Test/i)).toBeInTheDocument();
-// });
-
-// test("User selects a document",  () => {
-//     const fetchDoc = jest.fn();
-//     const docs = [{
-//         _id: '632ac6bd3147836191eda891',
-//         title: "Test",
-//         content: "Testar igen.."
-//     }];
-
-//     render(<Editor docs={docs} fetchDoc={fetchDoc} />);
-//     fireEvent.click(screen.getByText("Choose a document"));
-//     expect(screen.getByText(/Test/i));
-// });
-
-// test('check that "Create new" button calls the saveForm function on click', () => {
-//     const mockHandleClick = jest.fn();
-//     render(<Editor saveForm={mockHandleClick}/>);
-
-//     const button = screen.getByRole("button", {name: "Create new"});
-
-//     fireEvent.click(button);
-
-//     expect(mockHandleClick).toHaveBeenCalledTimes(1);
-//  });
-
-// test('test dropdown', () => {
-//     const onChange = jest.fn();
-//     const docs = [{
-//             "name": "Document 1",
-//             "content": "Documentcontent 1"
-//         },
-//         {
-//             "name": "Document 2",
-//             "content": "Documentcontent 2"
-//         }]
-//     const { getAllByTestId } =     render(<DropDown docs={docs} onChange={fetchDoc}/>);
-//     let options = getAllByTestId('select');
-//     expect(options).toHaveLength(2);
-// });
